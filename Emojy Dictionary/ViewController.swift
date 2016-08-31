fe27c7afa14fa6a499af3c08b1f8f578f43c0c62
magonicolas <magonicolas@gmail.com>
2016-08-31 14:21:34 -0300

    Tables View Protocols Complete

--- Emojy Dictionary/ViewController.swift
+++ Emojy Dictionary/ViewController.swift
@@ -8,16 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var emojyTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        emojyTableView.dataSource = self
+        emojyTableView.delegate = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "How u Doing?"
+        return cell
     }
 
 
